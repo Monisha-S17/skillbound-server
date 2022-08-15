97a553a8c53b76d13bdcd8cce8d0669abca8ba25
Monisha-S17 <moni17122000@gmail.com>
2022-08-15 09:27:46 +0530

    get sale skill data

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
+const fileUpload = require('express-fileupload');
+
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -18,7 +20,10 @@ app.use(express.json());
 app.use(cors(config));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(fileUpload());
 
+
+//get user details
 app.get("/userDetails/:userId", (req, res) => {
   connection.query(
     "SELECT * FROM `skill_users` WHERE id=394",
@@ -30,16 +35,15 @@ app.get("/userDetails/:userId", (req, res) => {
 });
 
 app.post("/dashboard", (req, res) => {
-  console.log(req);
+  // console.log(req);
 
   const emailId = req.body.email;
   const password = req.body.password;
 
-  console.log(emailId + "," + password);
 
   // connection.query(
   const sql = `SELECT * FROM skill_users WHERE emailid = "${emailId}" AND password = "${password}"`;
-  console.log(sql);
+  // console.log(sql);
 
   connection.query(sql, (err, result, fields) => {
     if (err) {
@@ -68,7 +72,6 @@ app.get("/subCategory/:categoryId", (req, res) => {
   // console.log(req.body);
 
   const categoryId = req.params.categoryId;
-  console.log(categoryId);
 
   // connection.query(
   const sql1 = `SELECT * FROM sub_cat WHERE cat_id = ${categoryId} `;
@@ -86,6 +89,7 @@ app.get("/subCategory/:categoryId", (req, res) => {
   //   res.json({results});
 });
 
+// category id  insert in skill_got table
 app.get("/skillCatIdUpdate", (req, res) => {
   connection.query(
     "select * From `categories` LIMIT 00, 800",
@@ -97,7 +101,6 @@ app.get("/skillCatIdUpdate", (req, res) => {
                      cat_id = ${catId} 
                      WHERE category = "${cat_name}" `;
         connection.query(sql, function (err, results, fields) {
-          console.log(sql);
           if (err) {
             console.log(err.message);
           } else {
@@ -110,6 +113,8 @@ app.get("/skillCatIdUpdate", (req, res) => {
   );
   // res.json({results});
 });
+
+// sub category id  insert in skill_got table
 
 app.get("/skillSubCatIdUpdate", (req, res) => {
   connection.query(
@@ -123,7 +128,6 @@ app.get("/skillSubCatIdUpdate", (req, res) => {
                      s_cat_id = ${s_cat_id} 
                      WHERE s_cat_name = "${s_cat_name}" AND cat_id = ${catId}  `;
         connection.query(sql, function (err, results, fields) {
-          console.log(sql);
           if (err) {
             console.log(err.message);
           } else {
@@ -137,6 +141,8 @@ app.get("/skillSubCatIdUpdate", (req, res) => {
   // res.json({results});
 });
 
+// category id  insert in skill_wants table
+
 app.get("/wantSkillCatIdUpdate", (req, res) => {
   connection.query(
     "select * From `categories` LIMIT 00, 800",
@@ -148,7 +154,6 @@ app.get("/wantSkillCatIdUpdate", (req, res) => {
                      cat_id = ${catId} 
                      WHERE category = "${cat_name}" `;
         connection.query(sql, function (err, results, fields) {
-          console.log(sql);
           if (err) {
             console.log(err.message);
           } else {
@@ -161,6 +166,8 @@ app.get("/wantSkillCatIdUpdate", (req, res) => {
   );
   // res.json({results});
 });
+
+// Sub category id  insert in skill_wants table
 
 app.get("/wantSkillSubCatIdUpdate", (req, res) => {
   connection.query(
@@ -174,7 +181,6 @@ app.get("/wantSkillSubCatIdUpdate", (req, res) => {
                      s_cat_id = ${s_cat_id} 
                      WHERE s_cat_name = "${s_cat_name}" AND cat_id = ${catId}  `;
         connection.query(sql, function (err, results, fields) {
-          console.log(sql);
           if (err) {
             console.log(err.message);
           } else {
@@ -212,17 +218,14 @@ app.get("/skills/:userId", (req, res) => {
 
 app.post("/addSkill", (req, res) => {
   const user_id = req.body.userId;
-  console.log(user_id);
   const cat_id = req.body.categoryId;
   const subCategoryId = req.body.subCategoryId;
   const skillLevel = req.body.skillLevel;
   const teachLevel = req.body.teachLevel;
   const wishes = req.body.wishes;
   const countryId = req.body.countryId;
-  //console.log(countryId);
 
   const sql = `INSERT INTO skill_got (user_id, cat_id, s_cat_id, level1, level2,level3, country) VALUES (${user_id},${cat_id}, "${subCategoryId}", "${skillLevel}","${teachLevel}", "${wishes}", ${countryId} )`;
-  console.log(sql);
   connection.query(sql, function (err, results, fields) {
     if (err) {
       console.log(err.message);
@@ -236,7 +239,6 @@ app.post("/addSkill", (req, res) => {
 //update user skills
 app.put("/skillUpdate", (req, res) => {
   const skillId = req.body.skillId;
-  console.log("id", skillId);
   const cat_id = req.body.categoryId;
   const selectWishes = req.body.selectWishes;
   const skillLevel = req.body.skillLevel;
@@ -261,7 +263,6 @@ app.delete("/delete/:skillId", (req, res) => {
   // console.log(req.body);
 
   const skillId = req.params.skillId;
-  console.log(skillId);
 
   // connection.query(
   const sql1 = `DELETE FROM skill_got WHERE id = ${skillId} `;
@@ -294,7 +295,6 @@ app.get("/wantSkill/:userId", (req, res) => {
     if (err) {
       console.log(err.message);
     } else {
-      console.log(results);
       res.json(results);
     }
   });
@@ -323,9 +323,137 @@ app.post("/signup", (req, res) => {
   );
 });
 
+//update user want skills
+app.put("/wantSkillUpdate", (req, res) => {
+  const skillId = req.body.wantSkillId;
+  const cat_id = req.body.categoryId;
+  const selectWishes = req.body.selectWishes; 
+  const subCategoryId = req.body.subCategoryId;
+
+  const sql = `UPDATE skill_wants SET cat_id=${cat_id}, s_cat_id= ${subCategoryId},  level="${selectWishes}" WHERE id= ${skillId}`;
+  connection.query(sql, function (err, results, fields) {
+
+    if (err) {
+      console.log(err.message);
+    } else {
+      // console.log(results);
+      res.json("Updated Successfully");
+    }
+  });
+});
+
+//Add user want skills
+
+app.post("/addWantSkill", (req, res) => {
+  const user_id = req.body.userId;
+  const cat_id = req.body.categoryId;
+  const subCategoryId = req.body.subCategoryId;  
+  const wishes = req.body.wishes;
+  const countryId = req.body.countryId;
+  //console.log(countryId);
+
+  const sql = `INSERT INTO skill_wants (user_id, cat_id, s_cat_id, level, country) VALUES (${user_id},${cat_id}, "${subCategoryId}", "${wishes}", ${countryId} )`;
+  connection.query(sql, function (err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    } else {
+      res.json("Want Skill Added Successfully");
+    }
+  });
+});
+
+//Delete want skill data
+app.delete("/WantSkilldelete/:wantSkillId", (req, res) => {
+
+  const wantSkillId = req.params.wantSkillId;
+
+  // connection.query(
+  const sql1 = `DELETE FROM skill_wants WHERE id = ${wantSkillId} `;
+  //console.log(sql1);
+
+  connection.query(sql1, function (err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    } else {
+      //console.log(results);
+      res.json("Skill is deleted");
+    }
+  });
+  // //  )
+  //   res.json({results});
+});
+
+
+//get skill sale data
+app.get("/getSkillSale/:userId", (req, res) => {
+  const currentUserId = req.params.userId;
+
+  const sql = `SELECT * FROM onlineclass_reg WHERE onlineclass_reg.user_id = ${currentUserId} 
+              `;
+
+  connection.query(sql, function (err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    } else {
+      // console.log(results);
+      res.json(results);
+    }
+  });
+});
+
+// add skill sale data
+// app.post("/addSaleSkill", (req, res) => {
+//   const user_id = req.body.userId;
+//   const className = req.body.className;
+//   const skillName = req.body.skillName;  
+//   const currency = req.body.currency;
+//   const payment = req.body.payment;
+//   const videoFile = req.body.videoFile;
+//   const serviceOffer = req.body.serviceOffer;  
+
+//   const sql = `INSERT INTO  onlineclass_reg (user_id, classname	, conductedby, currency,payment, videotutorial,skills) VALUES (${user_id},"${className}", "${serviceOffer}", "${currency}", "${payment}","${videoFile}","${skillName}" )`;
+//   console.log(sql);
+//   connection.query(sql, function (err, results, fields) {
+//     if (err) {
+//       console.log(err.message);
+//     } else {
+//       // console.log(results);
+//       res.json("Want Skill Added Successfully");
+//     }
+//   });
+// });
+
+app.post('/addSaleSkill', function(req, res) {
+  let sampleFile;
+  let uploadPath;
+  console.log(req.body.videoFile);
+
+  // if (!req.files || Object.keys(req.files).length === 0) {
+  //   return res.status(400).send('No files were uploaded.');
+  // }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.body.videoFile;
+  uploadPath = __dirname + '/upload/' + sampleFile;
+  console.log(uploadPath);
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
+
+
 app.post("/skills", (req, res) => {
   res.json({ data: "Welcome2" });
 });
+
+//file upload
+
+
 
 app.listen("3000", () => {
   console.log("Server started");
